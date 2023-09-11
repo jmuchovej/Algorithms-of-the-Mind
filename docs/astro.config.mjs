6e75eb4593424a17e8e3970cdc7a695c089e7bda
John Muchovej <5000729+jmuchovej@users.noreply.github.com>
2023-09-10 22:27:09 -0400

    ➕ Add `@astrojs/netlify`

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,28 +1,32 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://algorithms-of-the-mind.netlify.app",
-	server: {
-		host: "0.0.0.0",
-		port: 4261,
-	},
-	integrations: [
-		starlight({
-			title: "Algorithms of the Mind",
-			social: {
-				github: "https://github.com/cnclgithub/algorithms-of-the-mind",
-			},
-			sidebar: [
-				{
-					label: "Guides",
-					autogenerate: { directory: "guides" },
-				},
-				{ label: "Reference",
-					autogenerate: { directory: "reference" },
-				},
-			],
-		}),
-	],
+  server: {
+    host: "0.0.0.0",
+    port: 4261
+  },
+  integrations: [starlight({
+    title: "Algorithms of the Mind",
+    social: {
+      github: "https://github.com/cnclgithub/algorithms-of-the-mind"
+    },
+    sidebar: [{
+      label: "Guides",
+      autogenerate: {
+        directory: "guides"
+      }
+    }, {
+      label: "Reference",
+      autogenerate: {
+        directory: "reference"
+      }
+    }]
+  })],
+  output: "server",
+  adapter: netlify()
 });
